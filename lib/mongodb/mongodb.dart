85fd6d6f29e88864e6838879ec64112c56d8d1c7
Upendra-21 <upendrataral21@gmail.com>
2024-01-04 12:10:26 +0530

    Completed logic for adding new user and updating/apending loc of existing user

--- lib/mongodb/mongodb.dart
+++ lib/mongodb/mongodb.dart
@@ -9,34 +9,41 @@ class MongoDatabase {
     await db.open();
     inspect(db);
     var status = db.serverStatus();
-    print(status);
+    print("Status = " + status.toString());
 
     //Opening collection
     var collection = db.collection(COLLECTION_NAME);
 
     //Check existing user
-    var exist = await collection.find().toList();
+    var exist = await collection.find({'phone': '1234567890'}).toList();
 
-    for (var avertRecords in exist) {
-      if (avertRecords['exists'] == 'true') {
-        //Update records, [Multiple fields]
-        final filter = where.eq('phone', '1234567890');
-        final update = modify.set('latitude', '11').set('longitude', '11');
-        await collection.update(filter, update, multiUpdate: true);
-      } else {
-        //Insert Operation
-        await collection.insertOne({
-          "phone": "1234567890",
-          "address": "Shivaji Nagar, Pune",
-          "latitude": "18.530823",
-          "longitude": "73.847466",
-          // "date_time": "3/1/2024, 11:01:00",
-          "exists": "true"
-        });
-      }
+    if (exist.isEmpty) {
+      //USER DOESN'T EXIST, ADD NEW RECORD
+      await collection.insertOne({
+        "phone": "9689061841",
+        "address": "Pimple Gurav, Pune",
+        "latitude": "19.530823",
+        "longitude": "72.847466",
+        "date_time": "3/1/2024, 11:01:00",
+        "exists": "true"
+      });
+
+      var listAll = await collection.find().toList();
+      print("NEW RECORD = $listAll");
+    } else {
+      //USER EXIST'S, UPDATE CURRENT RECORD
+      print("USER EXITS");
+      print("BEFORE UPDATE = ${exist[0]}");
+
+      final filter = where.eq('phone', '1234567890');
+      final update = modify.set('latitude', '111111111').set('longitude', '1111');
+      await collection.update(filter, update, multiUpdate: true);
+
+      var userUpdated = await collection.find({'phone': '1234567890'}).toList();
+      print("AFTER UPDATE = ${userUpdated[0]}");
     }
 
-    //Fetch and display records
+    // Fetch and display all records
     print(await collection.find().toList());
   }
 }
